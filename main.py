3617d6a1bc4ce77139c9f64b6e7eb5e980981f47
Szymon Kryzel <szymonkryzel13@gmail.com>
2022-03-25 20:05:53 +0100

    add input fields

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ window.iconphoto(True, icon)
 
 # radio buttons
 var = IntVar()
-Radiobutton(window, text="Square signal", variable=var, value=0,).grid(row=0, column=0)
+Radiobutton(window, text="Square wave", variable=var, value=0,).grid(row=0, column=0)
 Radiobutton(window, text="Heaviside step function", variable=var, value=1,).grid(row=0, column=1)
 Radiobutton(window, text="Sine function", variable=var, value=2,).grid(row=0, column=2)
+
+# input fields
+Label(window, text="Amplitude:").grid(row=1, column=0)
+amplitude = Entry(window, width=10).grid(row=1, column=1)
+Label(window, text="Frequency:").grid(row=2, column=0)
+frequency = Entry(window, width=10).grid(row=2, column=1)
 
 window.mainloop()
