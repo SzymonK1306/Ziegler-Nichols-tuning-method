26098f20583a96b9c76741c76ed030eb3a34d196
Szymon Kryzel <szymonkryzel13@gmail.com>
2022-05-01 19:43:44 +0200

    Add frames to gui, add other parameters to gui

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ import numpy as np                      # lib for mathematical functions
 
 # creation the window
 window = Tk()
-window.geometry("1100x600")
+window.geometry("900x600")
 window.title("Ziegler–Nichols tuning method")
+
+# Frames
+image_frame = Frame(window)  # frame for image
+image_frame.pack()
+
+signal_title_frame = Frame(window)  # frame for the text on the signal choice
+signal_title_frame.pack()
+
+signal_frame = Frame(window)    # frame for signals to choose
+signal_frame.pack()
+
+parameters_title_frame = Frame(window)  # frame for the text on the parameters choice
+parameters_title_frame.pack()
+
+parameters_frame = Frame(window)
+parameters_frame.pack()
 
 # window icon
 icon = PhotoImage(file='images/pid.png')
@@ -14,21 +30,37 @@ window.iconphoto(True, icon)
 
 # photos
 schematic = ImageTk.PhotoImage(Image.open("images/schematic.png"))
-schematic_label = Label(image=schematic).grid(row=0, column=0)
-
+schematic_label = Label(image_frame, image=schematic)
+schematic_label.pack()
 
 # radio buttons
+Label(signal_title_frame, text="Choose input signal:", font=("Arial", 15)).pack()
 var = IntVar()
-Radiobutton(window, text="Square wave", variable=var, value=0,).grid(row=0, column=1)
-Radiobutton(window, text="Heaviside step function", variable=var, value=1,).grid(row=0, column=2)
-Radiobutton(window, text="Sine function", variable=var, value=2,).grid(row=0, column=3)
+Radiobutton(signal_frame, text="Square wave", variable=var, value=0,).grid(row=0, column=1)
+Radiobutton(signal_frame, text="Heaviside step function", variable=var, value=1,).grid(row=0, column=2)
+Radiobutton(signal_frame, text="Sine function", variable=var, value=2,).grid(row=0, column=3)
 
 # input fields
-Label(window, text="Amplitude:").grid(row=1, column=1)
-amplitude = Entry(window, width=10).grid(row=2, column=1)
-Label(window, text="Frequency:").grid(row=1, column=2)
-frequency = Entry(window, width=10).grid(row=2, column=2)
-Label(window, text="'a' parameter").grid(row=1, column=3)
-a_parameter = Entry(window,width=10).grid(row=2,column=3)
+Label(parameters_title_frame, text="Choose parameters:", font=("Arial", 15)).pack()
+# Amplitude
+Label(parameters_frame, text="Amplitude:").grid(row=1, column=1)
+amplitude = Entry(parameters_frame, width=10)
+amplitude.grid(row=2, column=1, padx=10)
+# Frequency
+Label(parameters_frame, text="Frequency:").grid(row=1, column=2)
+frequency = Entry(parameters_frame, width=10)
+frequency.grid(row=2, column=2, padx=10)
+# a parameter
+Label(parameters_frame, text="'a' parameter:").grid(row=1, column=3)
+a_parameter = Entry(parameters_frame, width=10)
+a_parameter.grid(row=2, column=3, padx=10)
+# gain 'k'
+Label(parameters_frame, text="gain 'k':").grid(row=1, column=4)
+gain_k = Entry(parameters_frame, width=10)
+gain_k.grid(row=2, column=4, padx=10)
+# Integral time
+Label(parameters_frame, text="Integral time 'T':").grid(row=1, column=5)
+integral_time = Entry(parameters_frame, width=10)
+integral_time.grid(row=2, column=5, padx=10)
 
 window.mainloop()
