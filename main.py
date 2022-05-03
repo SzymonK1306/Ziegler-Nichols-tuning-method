4b9d9be6ce1523be16e9d074f854276d1be571fe
tommikulevich <tommikulevich@gmail.com>
2022-05-03 15:49:59 +0200

    Added the function of State space, stability testing and input signals. Code reorganization

--- main.py
+++ main.py
@@ -2,6 +2,61 @@ from tkinter import *  # lib for GUI
 from PIL import ImageTk, Image
 from matplotlib import pyplot as plt  # lib for plots
 import numpy as np  # lib for mathematical functions
+import math
+
+
+# ------------- FUNCTIONS -------------
+
+# definition of a system in the space of states
+def state_space(a, k, T, zn_method):
+    if (zn_method):
+        k_max = 2 * (a ** 3)
+        T_osc = 2 * math.pi / a
+        k = 0.45 * k_max
+        T = 0.85 * T_osc
+
+    numerator = [0, 0, 0, k * T, k]
+    denominator = [1, 2 * a, a ** 2, k, k / T]
+
+    A = [[0, 1, 0, 0],
+         [0, 0, 1, 0],
+         [0, 0, 0, 1],
+         [-k / T, -k, -(a ** 2), -2 * a]]
+
+    B = [[0],
+         [0],
+         [0],
+         [1]]
+
+    C = [[k, k * T, 0, 0]]
+
+    return A, B, C
+
+
+def stability_test(a, k):
+    if (2 * a ** 3 > k):
+        return True
+    else:
+        return False
+
+
+def excitations(signal, total, duty_cycle, ampl, freq, period):
+    u = []
+
+    if signal == "Step":
+        for i in range(total):
+            u.append(ampl)
+    elif signal == "Square":
+        for i in range(total):
+            if (i < (total - 1) * duty_cycle):
+                u.append(ampl)
+            else:
+                u.append(-ampl)
+    elif signal == "Sine":
+        for i in range(total):
+            u.append(ampl * math.sin(2 * math.pi * freq * i * h))
+
+    return u
 
 
 # matrix multiplication
@@ -11,9 +66,9 @@ def multiply_matrices(mat1, mat2):
     else:
         result = [[0 for i in range(len(mat2[0]))] for j in range(len(mat1))]  # create empty matrix of result
 
-        for i in range(len(mat1)):                              # rows of matrix 1
-            for j in range(len(mat2[0])):                       # columns of matrix 2
-                for k in range(len(mat2)):                      # iteration through rows and columns
+        for i in range(len(mat1)):  # rows of matrix 1
+            for j in range(len(mat2[0])):  # columns of matrix 2
+                for k in range(len(mat2)):  # iteration through rows and columns
                     result[i][j] += mat1[i][k] * mat2[k][j]
         return result
 
@@ -26,19 +81,19 @@ window.geometry("900x600")
 window.title("Ziegler–Nichols tuning method")
 
 # creating frames
-image_frame = Frame(window)             # frame for image
+image_frame = Frame(window)  # frame for image
 image_frame.pack()
 
-signal_title_frame = Frame(window)      # frame for text when selecting a signal
+signal_title_frame = Frame(window)  # frame for text when selecting a signal
 signal_title_frame.pack()
 
-signal_frame = Frame(window)            # frame for signals to choose
+signal_frame = Frame(window)  # frame for signals to choose
 signal_frame.pack()
 
 parameters_title_frame = Frame(window)  # frame for text when selecting parameters
 parameters_title_frame.pack()
 
-parameters_frame = Frame(window)        # frame for parameters
+parameters_frame = Frame(window)  # frame for parameters
 parameters_frame.pack()
 
 # window icon
@@ -71,16 +126,19 @@ Label(parameters_frame, text="Frequency:").grid(row=1, column=2)
 frequency = Entry(parameters_frame, width=10)
 frequency.insert(END, "1")  # default value
 frequency.grid(row=2, column=2, padx=10)
+
 # 'a' parameter
 Label(parameters_frame, text="'a' parameter:").grid(row=1, column=3)
 a_parameter = Entry(parameters_frame, width=10)
 a_parameter.insert(END, "1")  # default value
 a_parameter.grid(row=2, column=3, padx=10)
+
 # gain 'k'
 Label(parameters_frame, text="gain 'k':").grid(row=1, column=4)
 gain_k = Entry(parameters_frame, width=10)
 gain_k.insert(END, "1")  # default value
 gain_k.grid(row=2, column=4, padx=10)
+
 # integral time 'T'
 Label(parameters_frame, text="Integral time 'T':").grid(row=1, column=5)
 integral_time = Entry(parameters_frame, width=10)
@@ -88,35 +146,29 @@ integral_time.insert(END, "1")  # default value
 integral_time.grid(row=2, column=5, padx=10)
 
 # ------------- MATHEMATICAL MODEL -------------
-pi = 3.14159265
 
 a = float(a_parameter.get())
 k = float(gain_k.get())
 T = float(integral_time.get())
 
-k_max = 2 * (a ** 3)
-T_osc = 2 * pi / a
+zn_method = False
 
-# Ziegler-Nichols PI parameters
-k_ZN = 0.45 * k_max
-T_ZN = 0.85 * T_osc
+time = 1
+h = 0.001
+total = int(time / h + 1)
 
-numerator_ZN = [0, 0, 0, k_ZN * T_ZN, k_ZN]
-denominator_ZN = [1, 2 * a, a * a, k_ZN, k_ZN / T_ZN]
+signal = "Sine"  # "Step" "Square" "Sine"
+ampl = 2
+duty = 50  # %
+freq = 5.45  # Hz
+duty_cycle = duty / 100
+period = 1 / freq
 
-A_ZN = [[0, 1, 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1],
-        [-k_ZN / T_ZN, -k_ZN, -a * a, -2 * a]]
+if not zn_method:
+    stability = stability_test(a, k)
 
-B_ZN = [[0],
-        [0],
-        [0],
-        [1]]
+A, B, C = state_space(a, k, T, zn_method)
 
-C_ZN = [[k_ZN, k_ZN * T_ZN, 0, 0]]
-
-m = multiply_matrices(C_ZN, B_ZN)
-print(m)
+u = excitations(signal, total, duty_cycle, ampl, freq, period)
 
 window.mainloop()
