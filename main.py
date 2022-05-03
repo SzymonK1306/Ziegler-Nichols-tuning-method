37a2fc962f731de69ede00fed47222e85f122346
tommikulevich <tommikulevich@gmail.com>
2022-05-03 12:18:09 +0200

    Small changes in the code and comments

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-from tkinter import *                   # lib for GUI
+from tkinter import *  # lib for GUI
 from PIL import ImageTk, Image
-from matplotlib import pyplot as plt    # lib for plots
-import numpy as np                      # lib for mathematical functions
+from matplotlib import pyplot as plt  # lib for plots
+import numpy as np  # lib for mathematical functions
 
 
-# multiply matrices
+# matrix multiplication
 def multiply_matrices(mat1, mat2):
-        if len(mat1[0])!=len(mat2):     # checking matrix dimensions
-                print("Wrong matrices")
-        else:
-                result = [[0 for i in range(len(mat2[0]))] for j in range(len(mat1))]   # create empty matrix of result
+    if len(mat1[0]) != len(mat2):  # checking matrix dimensions
+        print("Wrong matrices")
+    else:
+        result = [[0 for i in range(len(mat2[0]))] for j in range(len(mat1))]  # create empty matrix of result
 
-                for i in range(len(mat1)):      # rows of matrix 1
-                        for j in range(len(mat2[0])):   # columns of matrix 2
-                                for k in range(len(mat2)):      # iteration through rows and columns
-                                        result[i][j] += mat1[i][k] * mat2[k][j]
-                return result
+        for i in range(len(mat1)):                              # rows of matrix 1
+            for j in range(len(mat2[0])):                       # columns of matrix 2
+                for k in range(len(mat2)):                      # iteration through rows and columns
+                    result[i][j] += mat1[i][k] * mat2[k][j]
+        return result
 
 
-# GUI
+# ------------- GUI -------------
 
-# creation the window
+# creating a window
 window = Tk()
 window.geometry("900x600")
 window.title("Ziegler–Nichols tuning method")
 
-# Frames
-image_frame = Frame(window)  # frame for image
+# creating frames
+image_frame = Frame(window)             # frame for image
 image_frame.pack()
 
-signal_title_frame = Frame(window)  # frame for the text on the signal choice
+signal_title_frame = Frame(window)      # frame for text when selecting a signal
 signal_title_frame.pack()
 
-signal_frame = Frame(window)    # frame for signals to choose
+signal_frame = Frame(window)            # frame for signals to choose
 signal_frame.pack()
 
-parameters_title_frame = Frame(window)  # frame for the text on the parameters choice
+parameters_title_frame = Frame(window)  # frame for text when selecting parameters
 parameters_title_frame.pack()
 
 parameters_frame = Frame(window)        # frame for parameters
@@ -45,70 +45,76 @@ parameters_frame.pack()
 icon = PhotoImage(file='images/pid.png')
 window.iconphoto(True, icon)
 
-# photos
+# images
 schematic = ImageTk.PhotoImage(Image.open("images/schematic.png"))
 schematic_label = Label(image_frame, image=schematic)
 schematic_label.pack()
 
 # radio buttons
-Label(signal_title_frame, text="Choose input signal:", font=("Arial", 15)).pack()
 var = IntVar()
-Radiobutton(signal_frame, text="Square wave", variable=var, value=0,).grid(row=0, column=1)
-Radiobutton(signal_frame, text="Heaviside step function", variable=var, value=1,).grid(row=0, column=2)
-Radiobutton(signal_frame, text="Sine function", variable=var, value=2,).grid(row=0, column=3)
+Label(signal_title_frame, text="Choose input signal:", font=("Arial", 15)).pack()
+Radiobutton(signal_frame, text="Square wave", variable=var, value=0, ).grid(row=0, column=1)
+Radiobutton(signal_frame, text="Heaviside step function", variable=var, value=1, ).grid(row=0, column=2)
+Radiobutton(signal_frame, text="Sine function", variable=var, value=2, ).grid(row=0, column=3)
 
 # input fields
 Label(parameters_title_frame, text="Choose parameters:", font=("Arial", 15)).pack()
-# Amplitude
+
+# amplitude
 Label(parameters_frame, text="Amplitude:").grid(row=1, column=1)
 amplitude = Entry(parameters_frame, width=10)
-amplitude.insert(END, "1")      # default value
+amplitude.insert(END, "1")  # default value
 amplitude.grid(row=2, column=1, padx=10)
-# Frequency
+
+# frequency
 Label(parameters_frame, text="Frequency:").grid(row=1, column=2)
 frequency = Entry(parameters_frame, width=10)
-frequency.insert(END, "1")      # default value
+frequency.insert(END, "1")  # default value
 frequency.grid(row=2, column=2, padx=10)
-# a parameter
+# 'a' parameter
 Label(parameters_frame, text="'a' parameter:").grid(row=1, column=3)
 a_parameter = Entry(parameters_frame, width=10)
-a_parameter.insert(END, "1")    # default value
+a_parameter.insert(END, "1")  # default value
 a_parameter.grid(row=2, column=3, padx=10)
 # gain 'k'
 Label(parameters_frame, text="gain 'k':").grid(row=1, column=4)
 gain_k = Entry(parameters_frame, width=10)
-gain_k.insert(END, "1")         # default value
+gain_k.insert(END, "1")  # default value
 gain_k.grid(row=2, column=4, padx=10)
-# Integral time
+# integral time 'T'
 Label(parameters_frame, text="Integral time 'T':").grid(row=1, column=5)
 integral_time = Entry(parameters_frame, width=10)
-integral_time.insert(END, "1")    # default value
+integral_time.insert(END, "1")  # default value
 integral_time.grid(row=2, column=5, padx=10)
 
-# MATHEMATICAL MODEL
+# ------------- MATHEMATICAL MODEL -------------
+pi = 3.14159265
+
 a = float(a_parameter.get())
 k = float(gain_k.get())
 T = float(integral_time.get())
 
-k_max = 2*a*a*a
-T_osc = 2*3.14/a
+k_max = 2 * (a ** 3)
+T_osc = 2 * pi / a
 
 # Ziegler-Nichols PI parameters
-k_ZN = 0.45*k_max
-T_ZN = 0.85*T_osc
+k_ZN = 0.45 * k_max
+T_ZN = 0.85 * T_osc
 
-numerator_ZN = [0, 0, 0, k_ZN*T_ZN, k_ZN]
-denominator_ZN = [1, 2*a, a*a, k_ZN, k_ZN/T_ZN]
+numerator_ZN = [0, 0, 0, k_ZN * T_ZN, k_ZN]
+denominator_ZN = [1, 2 * a, a * a, k_ZN, k_ZN / T_ZN]
 
 A_ZN = [[0, 1, 0, 0],
         [0, 0, 1, 0],
         [0, 0, 0, 1],
-        [-k_ZN/T_ZN, -k_ZN, -a*a, -2*a]]
+        [-k_ZN / T_ZN, -k_ZN, -a * a, -2 * a]]
+
 B_ZN = [[0],
         [0],
         [0],
         [1]]
-C_ZN = [[k_ZN, k_ZN*T_ZN, 0, 0]]
+
+C_ZN = [[k_ZN, k_ZN * T_ZN, 0, 0]]
 
 m = multiply_matrices(C_ZN, B_ZN)
 print(m)
